b1d625e0658569db05927c318ec36e93d775a77b
adam09205 <adam.panczak50@gmail.com>
2026-04-09 19:05:06 +0200

    1

---
+++ Pwiz_lab3/Osoba.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
+    public string Stanowisko { get; set; }
+    // Konstruktor
+    public Person()
+    {
+        FirstName = "Brak Imienia";
+        LastName = "Brak Nazwiska";
+        Age = 0;
+        Stanowisko = "Brak Stanowiska";
+    }
+    public Person(string firstName, string lastName, int age, string stanowisko)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Age = age;
+        Stanowisko = stanowisko;
+    }
+
+    // Metoda do wyświetlania informacji o osobie
+    public void DisplayInfo()
+    {
+        Console.WriteLine("Imię: " + FirstName);
+        Console.WriteLine("Nazwisko: " + LastName);
+        Console.WriteLine("Wiek: " + Age);
+        Console.WriteLine("Stanowisko: " + Stanowisko);
+    }
+
+}
